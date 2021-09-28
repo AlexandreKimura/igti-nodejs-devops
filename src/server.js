351668a1abf1cdd90cff73a435614ea28aae53cd
Alexandre Kimura <alexandremkimura@hotmail.com>
2021-09-27 21:42:58 -0300

    fix server

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ const app = require("./app");
 
 app.listen(process.env.PORT || 5678, () => {
   console.log(
-    "Bootcamp desenvolvedor back end - Tópicos especiais. Aplicação de exemplo ouvindo na porta 5678!"
+    "Bootcamp desenvolvedor back end - Tópicos especiais. Aplicação de exemplo ouvindo em execução!"
   );
 });
